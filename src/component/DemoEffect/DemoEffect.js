53b366f94b7b0f28ad1281d0d51d8be7b5302411
xuanbao95 <bao.lgx@gmail.com>
2021-12-02 20:39:53 +0700

    2/12/2

--- src/component/DemoEffect/DemoEffect.js
+++ src/component/DemoEffect/DemoEffect.js
@@ -3,17 +3,21 @@ import { observer } from 'mobx-react-lite';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 import Switch from '@mui/material/Switch';
+import ColorPicker from 'polotno/toolbar/color-picker';
+import { NumericInput} from '@blueprintjs/core';
 export const DemoEffect = observer(({ store }) => {
     let elements = store.selectedElements[0];
     const [valid, setValid] = React.useState(false)
-    const [open, setOpen] = React.useState(false)
-    // for (const property in elements) {
-    //     console.log(`${property}: ${elements[property]}`);
-    // }
+    const [open, setOpen] = React.useState(false);
+    const [openBright,setOpenBright]=React.useState(false);
+    const[openStroke,setOpenStroke]=React.useState(false)
+    for (const property in elements) {
+        console.log(`${property}: ${elements[property]}`);
+    }
     /**
      * blur-done
-     * brightness
-     * seia
+     * brightness-done
+     * seia-not done yet
      * greyscale
      * text stroke
      * shadow
@@ -24,6 +28,25 @@ export const DemoEffect = observer(({ store }) => {
         elements.set({ blurEnabled: valid })
         elements.set({ blurRadius: eValue * 0.1 })
 
+    }
+    const handleChangeBright=(e)=>{
+        let eValue = e.target.value;
+        elements.set({ brightnessEnabled: valid });
+        elements.set({ brightness: eValue * 0.1});
+    }
+    const handleChangeOpenBright=()=>{
+        if (valid === false) {
+            setValid(true)
+        } else {
+            setValid(false)
+        }
+        if (openBright === false) {
+            setOpenBright(true)
+        } else {
+            setOpenBright(false)
+        }
+        elements.set({ brightnessEnabled: valid });
+        elements.set({ brightness: 0});
     }
     const handleChangeValid = () => {
         if (valid === false) {
@@ -36,8 +59,41 @@ export const DemoEffect = observer(({ store }) => {
         } else {
             setOpen(false)
         }
-        elements.set({ blurEnabled: valid })
-        elements.set({ blurRadius: 0})
+        elements.set({ blurEnabled: valid });
+        elements.set({ blurRadius: 0});
+        
+    }
+    const handleChangeSepia=()=>{
+        if(valid===false){
+            setValid(true)
+            elements.set({sepiaEnabled:true})
+        }else{
+            setValid(false)
+            elements.set({sepiaEnabled:false})
+        }
+    }
+    const handleChangeGreyScale=()=>{
+        if(valid===false){
+            setValid(true)
+            elements.set({grayscaleEnabled:true})
+        }else{
+            setValid(false)
+            elements.set({grayscaleEnabled:false})
+        }
+    }
+    const handleChangeStroke=()=>{
+        if (valid === false) {
+            setValid(true)
+        } else {
+            setValid(false)
+        }
+        if (openStroke === false) {
+            setOpenStroke(true)
+        } else {
+            setOpenStroke(false)
+        }
+        elements.set({ strokeWidth: 0 });
+        elements.set({ stroke: "black"});
     }
     const label = { inputProps: { 'aria-label': 'Switch demo' } };
     const showBlur = () => {
@@ -71,6 +127,87 @@ export const DemoEffect = observer(({ store }) => {
             )
         }
     }
+    const showBrightness=()=>{
+        if (openBright === true) {
+            return (
+                <div style={{ display: "block" }}>
+                    <Slider
+
+                        id="blr"
+                        size="small"
+                        defaultValue={0}
+                        aria-label="Small"
+                        valueLabelDisplay="auto"
+                        onChange={(e) => handleChangeBright(e)}
+                    />
+                </div>
+            )
+
+        } else {
+            return (
+                <div style={{ display: "none" }}>
+                    <Slider
+                        id="blr"
+                        size="small"
+                        defaultValue={0}
+                        aria-label="Small"
+                        valueLabelDisplay="auto"
+                        onChange={(e) => handleChangeBright(e)}
+                    />
+                </div>
+            )
+        }
+    }
+    const showTextStroke=()=>{
+        if(openStroke===true){
+            return(
+                <div style={{display:'block'}}>
+                    <ColorPicker
+        value={elements.stroke}
+        onChange={(stroke) =>
+          elements.set({
+            stroke,
+          })
+        }
+        store={store}
+      />
+      <NumericInput
+        onValueChange={(fontSize) => {
+          elements.set({ strokeWidth: fontSize });
+        }}
+        value={elements.strokeWidth}
+        style={{ width: '50px', marginLeft: '10px' }}
+        min={1}
+        max={40}
+      />
+                </div>
+            )
+        }else{
+            return(
+                <div style={{display:'none'}}>
+                    <ColorPicker
+        value={elements.stroke}
+        onChange={(stroke) =>
+          elements.set({
+            stroke,
+          })
+        }
+        store={store}
+      />
+      <NumericInput
+        onValueChange={(fontSize) => {
+          elements.set({ strokeWidth: fontSize });
+        }}
+        value={elements.fontSize}
+        style={{ width: '50px', marginLeft: '10px' }}
+        min={1}
+        max={40}
+      />
+                </div>
+            )
+        }
+        
+    }
     return (
         <div>
             <div>
@@ -84,8 +221,21 @@ export const DemoEffect = observer(({ store }) => {
                 </Box>
                 <Box width={150}>
                     <label>Brightness</label>
-                    <Switch {...label} onChange={handleChangeValid} />
-                        {showBlur()}
+                    <Switch {...label} onChange={handleChangeOpenBright} />
+                        {showBrightness()}
+                </Box>
+                <Box width={150}>
+                    <label>Sepia</label>
+                    <Switch {...label} onChange={handleChangeSepia} />
+                </Box>
+                <Box width={150}>
+                    <label>GreyScale</label>
+                    <Switch {...label} onChange={handleChangeGreyScale} />
+                </Box>
+                <Box width={150}>
+                    <label>Text stroke</label>
+                    <Switch {...label} onChange={handleChangeStroke} />
+                    {showTextStroke()}
                 </Box>
             
             </div>
